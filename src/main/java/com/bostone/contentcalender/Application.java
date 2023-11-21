8651d168a5f0a6c9086e17bfde9a4c7ecad95ee5
BOSTONE069 <bostoneochieng69@gmail.com>
2023-11-20 20:01:22 -0800

    Inserted a comment on the application code

--- src/main/java/com/bostone/contentcalender/Application.java
+++ src/main/java/com/bostone/contentcalender/Application.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+/**
+ * This is the main class for the Spring Boot application.
+ * It initializes and runs the application.
+ */
 @SpringBootApplication
 public class  Application {
 
