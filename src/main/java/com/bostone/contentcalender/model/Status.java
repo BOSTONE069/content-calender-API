af88f4238b838c02f48a0305b1a7b6887383e3db
BOSTONE069 <bostoneochieng69@gmail.com>
2023-11-21 15:50:05 +0300

    Generated the enum classes for the java content class

---
+++ src/main/java/com/bostone/contentcalender/model/Status.java
@@ -0,0 +1,8 @@
+package com.bostone.contentcalender.model;
+
+public enum Status {
+    IDEA,
+    IN_PROGRESS,
+    COMPLETED,
+    PUBLISHED,
+}
---
+++ src/main/java/com/bostone/contentcalender/model/Type.java
@@ -0,0 +1,10 @@
+package com.bostone.contentcalender.model;
+
+public enum Type {
+
+    ARTICLE,
+    VIDEO,
+    COURSE,
+    CONFERENCE_TALK
+
+}
